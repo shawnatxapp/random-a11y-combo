bbbd94b164bf50996fa4082b34fe41de0c0b6a96
Shawn Rice <shawn@xappmedia.com>
2018-06-18 13:37:22 -0400

    Add type declaration

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for random-a11y-combo x.x
+// Project: https://github.com/johno/random-a11y-combo/blob/master/index.js
+// Definitions by: Shawn Rice <https://github.com/shawnatxapp>
+
+export type randomA11yOutput = [string, string];
+
+export function randomA11yCombo (): randomA11yOutput;
+export as namespace randomA11y;
